2bd97ebc26010e33549ba830ffba15ed1361f387
ruthwekesa <wekesaruth569@gmail.com>
2022-08-16 18:50:54 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+mostProfitableNeighborhood = 'Chelsea';
+companyCeo = 'Susan Smith';
+
+function companyName() {
+    const companyName = 'Scuber';
+}
+
+function mostProfitableNeighborhood(Chelsea) {
+    let mostProfitableNeighborhood = 'Chelsea';
+}
+
+function companyCeo() {
+    let companyCeo = 'Susan Smith';
+}
